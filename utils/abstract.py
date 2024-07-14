fba0510997ca0daefa69898ec1546c7cdc72124d
RaffeLegend <oldgirafferiver@gmail.com>
2024-07-15 00:01:19 +0800

    [#2] func: support kandinsky3 and sdxl refiner

--- utils/abstract.py
+++ utils/abstract.py
@@ -1,16 +1,19 @@
 from utils.utils import save_image, is_folder
 import torch
-from diffusers import AutoPipelineForText2Image, StableDiffusion3Pipeline, StableCascadeDecoderPipeline, StableCascadePriorPipeline
+from diffusers import AutoPipelineForText2Image, StableDiffusion3Pipeline, \
+                      StableCascadeDecoderPipeline, StableCascadePriorPipeline, \
+                      Kandinsky3Pipeline, DiffusionPipeline
+                      
 
 
 # define abstract class
 class StableDiffusion:
     def __init__(self) -> None:
-        self.prompt = None
+        self.prompt_set = None
         self.height = None
         self.width  = None
-        self.image  = None
-        self.output = None
+        self.model_name  = None
+        self.model_path = None
     
     def get_save_path(self):
         folder_name = "./" + self.model_name+ "_output"
@@ -158,6 +161,77 @@ class StableDiffusionCascade(StableDiffusion):
             save_image(decoder_output, self.save_path, index)
             index += 1
         return 
+    
+# define model sd cascade
+class Kandinsky3(StableDiffusion):
+    def __init__(self, model_name):
+        super().__init__()
+        self.prompt_set = None
+        self.model_name = model_name
+        self.model_path = "kandinsky-community/kandinsky-3"
+        self.torch_dtype = torch.float16
+        self.variant = "fp16"
+        self.model = self.init_model()
+        self.save_path = self.get_save_path()
+        self.negative_prompt = ""
+
+    def init_model(self):
+        self.pipeline = Kandinsky3Pipeline.from_pretrained(self.model_path, variant=self.variant, torch_dtype=self.torch_dtype)
+        self.pipeline.enable_model_cpu_offload()
+
+    def inference(self):
+        for prompt in self.prompt_set:
+            image = self.pipeline(prompt).images[0]
+            save_image(image, self.save_path, index)
+            index += 1
+        return 
+
+# define model sd cascade
+class StableDiffusionXLwithRefiner(StableDiffusion):
+    def __init__(self, model_name):
+        super().__init__()
+        self.prompt_set = None
+        self.model_name = model_name
+        self.model_base_path = "stabilityai/stable-diffusion-xl-base-1.0"
+        self.model_refiner_path = "stabilityai/stable-diffusion-xl-refiner-1.0"
+        self.torch_dtype = torch.float16
+        self.variant = "fp16"
+        self.model = self.init_model()
+        self.save_path = self.get_save_path()
+        self.negative_prompt = ""
+
+    def init_model(self):
+        self.base = DiffusionPipeline.from_pretrained(
+                            self.model_base_path, torch_dtype=self.torch_dtype, 
+                            variant=self.variant, use_safetensors=True
+                            ).to("cuda")
+
+        self.refiner = DiffusionPipeline.from_pretrained(
+                            self.model_refiner_path,
+                            text_encoder_2=self.base.text_encoder_2,
+                            vae=self.base.vae,
+                            torch_dtype=self.torch_dtype,
+                            use_safetensors=True,
+                            variant=self.variant,
+                            ).to("cuda")
+
+    def inference(self):
+        for prompt in self.prompt_set:
+            image = self.base(
+                        prompt=prompt,
+                        num_inference_steps=40,
+                        denoising_end=0.8,
+                        output_type="latent",
+                        ).images
+            image = self.refiner(
+                        prompt=prompt,
+                        num_inference_steps=40,
+                        denoising_start=0.8,
+                        image=image,
+                        ).images[0]
+            save_image(image, self.save_path, index)
+            index += 1
+        return 
 
 # model factory
 class ModelFactory:
@@ -171,5 +245,9 @@ class ModelFactory:
             return StableDiffusion3Medium(model_name=model_name)
         elif model_name == "sd_cascade":
             return StableDiffusionCascade(model_name=model_name)
+        elif model_name == "kandinsky3":
+            return Kandinsky3(model_name=model_name)
+        elif model_name == "sdxl_refiner":
+            return StableDiffusionXLwithRefiner(model_name=model_name)
         else:
             raise ValueError(f"Unknown model name: {model_name}")
