101f7890f36bb08de37bc89cfb3fa368bce3892a
RaffeLegend <oldgirafferiver@gmail.com>
2024-07-29 21:45:22 +0800

    [#3] fix: fix invalid params

--- run_stable_lm.py
+++ run_stable_lm.py
@@ -9,8 +9,7 @@ def parse_args():
     parser = argparse.ArgumentParser(description="Model Training and Evaluation Framework")
     parser.add_argument("--models", nargs='+', type=str, required=True, 
                         help="choose model from [SL2_12B]")
-    # parser.add_argument("--model", type=str, default="sdxl-turbo", help="model list")
-    parser.add_argument("--prompt",  type=str, default="prompt.json", help="prompt path")
+    # parser.add_argument("--prompt",  type=str, default="prompt.json", help="prompt path")
     parser.add_argument("--output_path",  type=str, default="./output", help="output path")
     parser.add_argument("--image_path",  type=str, default="", help="input image path")
     parser.add_argument("--data_name", type=str, default="used", help="name of dataset")
@@ -24,7 +23,6 @@ if __name__ == "__main__":
     model_list  = args.models
     image_path  = args.image_path
     output_path = args.output_path
-    prompt_path = args.prompt
     data_name   = args.data_name
     save_size   = args.save_batch_size
 
@@ -37,7 +35,7 @@ if __name__ == "__main__":
             model = LMModelFactory.get_model(model)
 
             # running pipeline to generate
-            model.get_save_path(output_path, prompt_path, data_name)
+            model.get_save_path(output_path, data_name)
             model.get_images_path(image_path, save_size)
             model.init_model()
             model.inference()
