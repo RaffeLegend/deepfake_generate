c2689b55d693bb76649755875bfc40b70f486f99
RaffeLegend <oldgirafferiver@gmail.com>
2024-10-15 20:04:18 +0800

    [#6] func: fix the model sdxl_lightning init bug

--- models/image_model/sdxl_lightning.py
+++ models/image_model/sdxl_lightning.py
@@ -1,6 +1,8 @@
 import torch
 from utils.tools import save_image
-from diffusers import DiffusionPipeline
+from diffusers import StableDiffusionXLPipeline, UNet2DConditionModel, EulerDiscreteScheduler
+from huggingface_hub import hf_hub_download
+from safetensors.torch import load_file
 
 from sd_embed.embedding_funcs import get_weighted_text_embeddings_sd3
 from prompts.prompt import NEGATIVE_PROMPT, PROMPT_REALISTIC_VISION_NEGATIVE
@@ -19,14 +21,16 @@ class SDXLLightning(DiffusionModel):
         self.variant = "fp16"
         # self.save_path = self.get_save_path()
         self.negative_prompt = ""
+        self.base = "stabilityai/stable-diffusion-xl-base-1.0"
+        self.ckpt = "sdxl_lightning_4step_unet.safetensors" 
 
     def init_model(self):
-        self.model = DiffusionPipeline.from_pretrained(
-                                        self.model_path,
-                                        torch_dtype=self.torch_dtype,
-                                        variant=self.variant,
-                                        ).to("cuda")
-        
+        self.unet = UNet2DConditionModel.from_config(self.base, subfolder="unet").to("cuda", torch.float16)
+        self.unet.load_state_dict(load_file(hf_hub_download(self.model, self.ckpt), device="cuda"))
+        self.model = StableDiffusionXLPipeline.from_pretrained(self.base, unet=self.unet, torch_dtype=torch.float16, variant="fp16").to("cuda")
+
+        self.model.scheduler = EulerDiscreteScheduler.from_config(self.model.scheduler.config, timestep_spacing="trailing")
+
         # self.set_prompt_enhancer()
 
     def inference(self):
@@ -38,8 +42,8 @@ class SDXLLightning(DiffusionModel):
                 prompt = data_info["prompt"]
                 prompt = self.prompt_process(prompt, NEGATIVE_PROMPT)
                 image = self.model(prompt=prompt, 
-                                   num_inference_steps=50, 
-                                   guidance_scale=3
+                                   num_inference_steps=4, 
+                                   guidance_scale=0
                                    ).images[0]
                 save_image(image, output_path, index)
         return 
